2a82038e1c983c732dd6124ffcc4f2a5c17b140e
Kyle Wilkins <Kyle.Wilkins@bbd.co.za>
2025-03-15 14:41:17 +0200

    Intial Setup

---
+++ src/main/java/com/bbdgrads/kudos_cli/KudosCliApplication.java
@@ -0,0 +1,13 @@
+package com.bbdgrads.kudos_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudosCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudosCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbdgrads/kudos_cli/KudosCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbdgrads.kudos_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudosCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
