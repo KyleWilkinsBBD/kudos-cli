4d48ce3cbaa9324e1067fdcd4d35adaaca05899d
Bernard Paetzold <bernard.paetzold@bbd.co.za>
2025-03-19 21:31:33 +0200

    Added docs

--- src/main/java/com/bbdgrads/kudos_cli/shell/UserShell.java
+++ src/main/java/com/bbdgrads/kudos_cli/shell/UserShell.java
@@ -15,60 +15,52 @@ public class UserShell {
     private final UserService userService;
     private final UserSession userSession;
 
-    public UserShell(AuthState authState, UserService userService, UserSession userSession){
+    public UserShell(AuthState authState, UserService userService, UserSession userSession) {
         this.userService = userService;
         this.authState = authState;
         this.userSession = userSession;
     }
 
-    @ShellMethod(key="token")
-    public String displayJwtToken(){
+    @ShellMethod(key = "token")
+    public String displayJwtToken() {
         return authState.getAPI_KEY();
     }
 
-    @ShellMethod(key="create_user",value = "This allows an admin to create a user")
+    @ShellMethod(key = "create_user", value = "Create a new user (admin only) - create_user [username] [google_id]")
     public String createUser(
             @ShellOption(help = "Username of the new user") String username,
-            @ShellOption(help = "Google ID of the new user") String googleId)
-    {
+            @ShellOption(help = "Google ID of the new user") String googleId) {
         return userService.createUser(username, googleId);
     }
 
-    @ShellMethod(key="my_team")
-    public String getTeam(){
+    @ShellMethod(key = "my_team", value = "Print the team you are assigned to")
+    public String getTeam() {
         return userService.getTeamOfUser(userSession.getUsername());
     }
 
-    @ShellMethod(key="users_in_team")
+    @ShellMethod(key = "users_in_team", value = "Print the users assigned to a specific team - users_in_team [team_name]")
     public String getUsersInTeam(
-            @ShellOption(help = "Name of the team") String team_name
-    ){
+            @ShellOption(help = "Name of the team") String team_name) {
         return userService.getAllUsersInATeam(team_name);
     }
 
-    @ShellMethod(key="upgrade_to_admin")
+    @ShellMethod(key = "upgrade_to_admin", value = "Upgrade a user to admin status - upgrade_to_admin [username]")
     public String updateToAdmin(
-            @ShellOption(help = "Username") String username
-    ){
+            @ShellOption(help = "Username") String username) {
         return userService.updateToAdmin(username);
     }
 
-    @ShellMethod(key="find_users_team")
+    @ShellMethod(key = "find_users_team", value = "Print the team a specified user belongs to - find_users_team [username]")
     public String findUsersTeam(
-            @ShellOption(help = "Username") String username
-    ){
+            @ShellOption(help = "Username") String username) {
         return userService.getTeamOfUser(username);
     }
 
-    @ShellMethod(key="add_user_to_team")
+    @ShellMethod(key = "add_user_to_team", value = "Add a user to a team - add_user_to_team [username] [team_name]")
     public String addUserToTeam(
             @ShellOption(help = "Username") String username,
-            @ShellOption(help = "Team name") String team_name
-    ){
+            @ShellOption(help = "Team name") String team_name) {
         return userService.addUserToTeam(username, team_name);
     }
-
-
-
 
 }
